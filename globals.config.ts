d69ea2deca09eb180d2fc890dc5195c2fbcbf0b4
quachtridat <quachtridat@yahoo.com.vn>
2022-08-12 02:42:02 -0700

    Use the new email address

--- globals.config.ts
+++ globals.config.ts
@@ -1,6 +1,6 @@
 import path from 'path';
 
-export const EMAIL_ADDRESS = "dattriquach1702@gmail.com"
+export const EMAIL_ADDRESS = "me@dquach.dev"
 export const GITHUB_URL = "https://github.com/quachtridat"
 export const LINKEDIN_URL = "https://linkedin.com/in/datquach"
 export const HACKERRANK_URL = "https://hackerrank.com/quachtridat"
